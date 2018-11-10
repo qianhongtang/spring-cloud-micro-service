131d16b0a1eccd91980ec40bf5242d5c59ed2e3b
qianhongtang <qianhongtang@gmail.com>
2018-11-10 21:45:44 +0800

    加入oauth2资源服务器配置

--- consul-gateway-zuul-oauth2-server/src/main/java/com/springcloud/zuul/server/ConsulGetwayZuulOauth2ServerApplication.java
+++ consul-gateway-zuul-oauth2-server/src/main/java/com/springcloud/zuul/server/ConsulGetwayZuulOauth2ServerApplication.java
@@ -3,9 +3,6 @@ package com.springcloud.zuul.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-
-import com.springcloud.zuul.server.filter.AccessZuulFilter;
 
 @EnableZuulProxy
 @SpringCloudApplication
@@ -15,8 +12,4 @@ public class ConsulGetwayZuulOauth2ServerApplication {
 		SpringApplication.run(ConsulGetwayZuulOauth2ServerApplication.class, args);
 	}
 
-	@Bean
-	public AccessZuulFilter accessZuulFilter() {
-		return new AccessZuulFilter();
-	}
 }
